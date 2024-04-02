994f25695cce0d503dfa69540236323ec4c05b33
Gabriele174 <162124980+Gabriele174@users.noreply.github.com>
2024-04-02 22:58:11 +0200

    Update Script.js

--- Script/Script.js
+++ Script/Script.js
@@ -1,1 +1,7 @@
-
+Function dologin ()  {
+  var username = document.getElementById("username").value;
+  var psw = document.getElementById("password").value;
+  if(username == "" || psw == "") {
+      alert("username e password obbligatori")
+  }
+}
