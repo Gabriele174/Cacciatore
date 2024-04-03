675ef614e427c11e2f8ab65d48c40542d9b61091
Gabriele174 <162124980+Gabriele174@users.noreply.github.com>
2024-04-03 10:25:34 +0200

    1

--- Script/Script.js
+++ Script/Script.js
@@ -1,7 +1,7 @@
-function doLogin() {
+document.getElementById("loginButton").addEventListener("click", function() {
   var username = document.getElementById("exampleInputEmail1").value;
   var password = document.getElementById("exampleInputPassword1").value;
   if(username === "" || password === "") {
-      alert("Username and password are required.");
+    alert("Username and password are required.");
   }
-}
+});
