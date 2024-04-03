01bfa4f37b49392f4f626ddf784a2ff283fea8d8
Gabriele174 <162124980+Gabriele174@users.noreply.github.com>
2024-04-03 10:01:18 +0200

    3

--- Script/Script.js
+++ Script/Script.js
@@ -1,7 +1,7 @@
-Function dologin ()  {
+function doLogin() {
   var username = document.getElementById("username").value;
-  var psw = document.getElementById("password").value;
-  if(username == "" || psw == "") {
-      alert("username e password obbligatori")
+  var password = document.getElementById("password").value;
+  if(username === "" || password === "") {
+      alert("Username and password are required.");
   }
 }
